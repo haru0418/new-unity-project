126fb76b9fdede120401620e448c4b3a47788916
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-10 23:29:54 +0900

    別オブジェクトの操作方法

---
+++ Assets/HPplayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPplayer : MonoBehaviour
+{
+    //public GameObject hpGameObject;
+    public Text hpText;
+    void Start()
+    {
+        // Text text = hpGameObject.GetComponent<Text>();
+        //text.text = "50";
+        hpText.text = "50";
+    }
+}
